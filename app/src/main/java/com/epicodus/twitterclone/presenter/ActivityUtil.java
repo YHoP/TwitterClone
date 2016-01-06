29f07b87d8b3529c9c65a9d36f460349b2a62c4d
YHop <ypeng.tw@gmail.com>
2016-01-05 18:35:31 -0800

    Add ActivituUtil for testing

---
+++ app/src/main/java/com/epicodus/twitterclone/presenter/ActivityUtil.java
@@ -0,0 +1,19 @@
+package com.epicodus.twitterclone.presenter;
+
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.epicodus.twitterclone.ui.MainActivity;
+
+public class ActivityUtil {
+    private Context context;
+
+    public ActivityUtil(Context context) {
+        this.context = context;
+    }
+
+    public void startMainActivity() {
+        context.startActivity(new Intent(context, MainActivity.class));
+    }
+}
